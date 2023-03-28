7cc8120a1802cf931bb350de2b5474e3e308ceee
Logan <logan24378@live.ytech.edu>
2023-03-28 14:39:09 -0400

    This Should work

--- Game_script.js
+++ Game_script.js
@@ -1,5 +1,6 @@
 
 
+
 class Sprite {
     constructor({ position, velocity, height, color = 'red' }) {
         this.height = 150
@@ -17,21 +18,61 @@ class Sprite {
         }
         this.color = color
     }
+    jump() {
+        console.log(isOnGround);
+            if (jumpcounter < 2) {
+            player1.velocity.y = -18
+            jumpcounter++
+            isOnGround = false
+        }
+
+    }
+    jump1(){
+        console.log(isOnGround1);
+        if (jumpcounter1<2){
+            enemy.velocity.y = -19
+            jumpcounter1++
+            isOnGround1 = false
+        }
+    }
     draw() {
         v.fillStyle = this.color
         v.fillRect(this.position.x, this.position.y, 40, 150, this.height)
     }
     update() {
-
-        this.draw()
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
-            this.velocity.y = 0
-        }
+        player1.draw()
+        player1.position.x += player1.velocity.x
+        player1.position.y += player1.velocity.y
+            
         
+        if (player1.position.y + player1.height + player1.velocity.y >= canvas.height - 10) {
+            player1.velocity.y = 0
+            isOnGround = true
+
+            
+        }
+        else if(keys.s.pressed && lastKey ==='s'){
+            player1.velocity.y += gravity + 2
+    
+        }
         else {
-            this.velocity.y += gravity
+            player1.velocity.y += gravity 
+        }
+    }
+    update1(){
+        enemy.draw()
+        enemy.position.x += enemy.velocity.x
+        enemy.position.y += enemy.velocity.y
+        if (enemy.position.y + enemy.height + enemy.velocity.y >= canvas.height - 10) {
+            enemy.velocity.y = 0
+            isOnGround1 = true
+            
+        }
+        else if(keys.ArrowDown.pressed && enemy.lastKey ==='ArrowDown'){
+            enemy.velocity.y += gravity1 + 0.5
+        }
+        else {
+            enemy.velocity.y += gravity1 
         }
     }
     attackBox() {
@@ -49,9 +90,14 @@ class Sprite {
 
     }
 }
-const gravity = 0.6
+const gravity = 0.5
+const gravity1 = 0.4
 const canvas = document.querySelector('canvas')
 const v = canvas.getContext('2d');
+var jumpcounter = 0
+var jumpcounter1 = 0
+var isOnGround = false
+var isOnGround1 = false
 const player1 = new Sprite({
     position: {
 
@@ -59,7 +105,7 @@ const player1 = new Sprite({
         y: 0
     },
     velocity: {
-        x: 0,
+        x: 5,
         y: 2.5
     },
     color: 'Yellow'
@@ -72,16 +118,16 @@ const enemy = new Sprite({
         y: 100
     },
     velocity: {
-        x: 0,
-        y: 2.5
+        x: 2,
+        y: 2.4
     },
-    color: 'blue'
+    color: 'orange'
 })
-canvas.width = 1800
-canvas.height = 900
+canvas.width =1900;
+canvas.height =950;
 v.fillRect(0, 0, canvas.width, canvas.height)
 
-console.log(player1)
+console.log(player1,enemy)
 
 const keys = {
     a: {
@@ -98,6 +144,9 @@ const keys = {
     },
     ArrowUp: {
         pressed: false
+    },
+    ArrowDown:{
+        pressed:false
     },
     p: {
         pressed: false
@@ -118,33 +167,48 @@ const keys = {
         pressed: false
     }
 }
+function d(){
+    var nineFrames
+    nineFrames++
+    return nineFrames
+}
 var lastKey
 function animate() {
     v.fillStyle = 'black'
     window.requestAnimationFrame(animate)
     v.fillRect(0, 0, canvas.width, canvas.height)
+    if (isOnGround === true ) {
+        jumpcounter = 0
+    }
+    if (isOnGround1 === true ) {
+        jumpcounter1 = 0
+    }
+    var frames 
+    var nineFrames
+    frames++
     player1.update()
-    enemy.update()
+    enemy.update1()
     player1.velocity.x = 0
     enemy.velocity.x = 0
+    
     //player 1 movement
     if (keys.a.pressed && lastKey === 'a') {
-        player1.velocity.x = -6
+        player1.velocity.x = -15
     } else if (keys.d.pressed && lastKey === 'd') {
-        player1.velocity.x = 6
+        player1.velocity.x = 15
     }
     //else if (keys.w.pressed && lastKey ==='w'){
     //  player1.velocity.y = 1
     //}
     //Enemy movements
     if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
-        enemy.velocity.x = -6
+        enemy.velocity.x = -10
     } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
-        enemy.velocity.x = 6
+        enemy.velocity.x = 10
     }
     if (keys.p.pressed && lastKey === 'p') {
+          player1.attackBox()
         
-        player1.attackBox() 
     }
 }
 animate()
@@ -160,14 +224,18 @@ window.addEventListener('keydown', (event) => {
             lastKey = 'a'
             break
         case 'w':
-            player1.velocity.y = -17
-            let
+            keys.w.pressed=true
+            player1.jump()
             break
+
         case 'p':
             keys.p.pressed = true
             lastKey = 'p'
             console.log(lastKey)
             break
+            case 's':
+                keys.s.pressed=true
+                lastKey='s'
     }
     switch (event.key) {
 
@@ -182,7 +250,8 @@ window.addEventListener('keydown', (event) => {
 
             break
         case 'ArrowUp':
-            enemy.velocity.y = -17
+            keys.ArrowDown.pressed=true
+            enemy.jump1()
             break
         case 'z':
             keys.z.pressed = true
@@ -192,6 +261,11 @@ window.addEventListener('keydown', (event) => {
             keys.x.pressed = true
             enemy.lastKey = 'x'
             break
+            case 'ArrowDown':
+                keys.ArrowDown.pressed = true
+                enemy.lastKey = 'ArrowDown'
+                break
+            
     }
 
 })
@@ -212,6 +286,9 @@ window.addEventListener('keyup', (event) => {
         case 'o':
             keys.o.pressed = false
             break
+        case 's':
+            keys.s.pressed=false
+            break
 
 
     }
@@ -228,6 +305,9 @@ window.addEventListener('keyup', (event) => {
             break
         case 'z':
             keys.z.pressed = false
+            break
+            case 'ArrowDown':
+            keys.ArrowDown.pressed = false
             break
 
 
